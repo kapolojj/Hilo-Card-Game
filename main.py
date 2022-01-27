8054d8179ba68bd28347b0ab7166085ae2a956ec
kapolojj <johanneskapolok@gmail.com>
2022-01-27 08:50:48 +0200

    error

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 """The program must include a README file.
 The program must include class and method comments.
 The program must have at least two classes.
-The program must remain true to game play described in the overview.""""
+The program must remain true to game play described in the overview."""
